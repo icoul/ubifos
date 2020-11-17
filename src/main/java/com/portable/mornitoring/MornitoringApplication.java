33f6842110254f88157f717177ee2e89bd7ff0a1
unknown <icoul35@gmail.com>
2020-11-17 16:32:25 +0900

    프로젝트 초기화

---
+++ src/main/java/com/portable/mornitoring/MornitoringApplication.java
@@ -0,0 +1,13 @@
+package com.portable.mornitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MornitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MornitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portable/mornitoring/MornitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portable.mornitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MornitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
