3e3681a32387b17245bd7d578e975cc77ac8abc0
unknown <icoul35@gmail.com>
2020-11-19 15:01:09 +0900

    각 페이지별 라우터 추가

--- frontend/src/Component/Main.js
+++ frontend/src/Component/Main.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
+
+import Control from './control/Control';
+import TableData from './tableData/TableData';
+import GraphData from './graphData/GraphData';
 
 import { MainContainer } from './MainContainer.css';
 
 const Main = () => {
   return (
     <MainContainer>
-
+      <Switch>
+        <Route
+          exact path="/"
+          render={props => <Control {...props} />} />
+        <Route
+          exact path="/table"
+          render={props => <TableData {...props} />} />
+        <Route
+          exact path="/graph"
+          render={props => <GraphData {...props} />} />
+      </Switch>
     </MainContainer>
   )
 }
