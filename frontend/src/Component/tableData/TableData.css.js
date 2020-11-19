120e2d716cc025aa528856b38f6a3a0db9000ce7
unknown <icoul35@gmail.com>
2020-11-19 16:02:34 +0900

    테이블 상세 데이터 초기화

---
+++ frontend/src/Component/tableData/TableData.css.js
@@ -0,0 +1,6 @@
+import style from 'styled-components'
+
+export const TableDataContainer = style.div`
+  padding-top: 1vh;
+  background: #292929;
+`
---
+++ frontend/src/Component/tableData/TableData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { TableDataContainer } from './TableData.css';
+
+const TableData = () => {
+  return (
+    <TableDataContainer>
+      <div>aaaaaaaaaaaa</div>
+    </TableDataContainer>
+  )
+}
+
+export default TableData;
