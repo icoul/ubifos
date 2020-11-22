ca7bdac2592560ca120a6aaeb3e4b0db8d34102f
unknown <icoul35@gmail.com>
2020-11-22 15:23:23 +0900

    페이지 URL에 따라 최상단 타이틀 변경

--- frontend/src/Component/AppHeader.js
+++ frontend/src/Component/AppHeader.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaHome, FaList, FaPowerOff } from 'react-icons/fa';
 import { BsGraphUp } from 'react-icons/bs';
+import { TiDocumentText } from 'react-icons/ti';
 
 import { AppHeaderContainer } from './MainContainer.css';
 
 import ci from 'static/images/logo.png'
 
+const titleMap = {
+  '/': '가스농도 현황',
+  '/table': '상세 데이터',
+  '/graph': '상세 그래프',
+  '/log': '로그 데이터',
+}
+
 const AppHeader = ( props ) => {
+  const [title, setTitle] = useState('가스농도 현황');
+
+  useEffect(() => {
+    setTitle(titleMap[props.history.location.pathname]);
+  }, [props.history.location])
+
   const moveLinkPage = (link) => {
     props.history.push(link);
   }
@@ -14,7 +28,7 @@ const AppHeader = ( props ) => {
   return (
     <AppHeaderContainer>
       <div className="header_logo"><img src={ci} alt="ci" /></div>
-      <div className="header_title">가스농도 현황</div>
+      <div className="header_title">{ title }</div>
       <div className="header_buttons">
         <div className="button_box" onClick={ () => { moveLinkPage('/') } }>
           <FaHome />
@@ -24,6 +38,9 @@ const AppHeader = ( props ) => {
         </div>
         <div className="button_box" onClick={ () => { moveLinkPage('/graph') } }>
           <BsGraphUp />
+        </div>
+        <div className="button_box" onClick={ () => { moveLinkPage('/log') } }>
+          <TiDocumentText />
         </div>
         <div className="button_box" onClick={ () => { moveLinkPage('/') } }>
           <FaPowerOff />
