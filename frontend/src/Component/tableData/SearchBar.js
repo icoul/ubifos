db71753f1906edc1eb4c27405fd05d2037d3ade1
unknown <icoul35@gmail.com>
2020-12-01 17:20:01 +0900

    가스 상세 데이터 CSV 파일 다운로드 기능 추가

--- frontend/src/Component/tableData/SearchBar.js
+++ frontend/src/Component/tableData/SearchBar.js
@@ -1,18 +1,53 @@
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
+import axios from 'axios';
+import { CSVLink } from "react-csv";
+
 import SearchDate from './SearchDate';
 import SearchModule from './SearchModule';
+import { getYMDAndHourFormatDate } from 'utils/getCustomFormatDate';
 
 import { SearchBarConatiner } from './SearchBar.css';
 
-const SearchBar = ({ setSearchMap }) => {
+const headers = [
+  { label: '장치명', key: 'modelNm' },
+  { label: 'O₂', key: 'o2' },
+  { label: 'CO₂', key: 'co2' },
+  { label: 'CO', key: 'co' },
+  { label: 'H₂S', key: 'h2s' },
+  { label: 'CH₄', key: 'ch4' },
+  { label: '배터리', key: 'battery' },
+  { label: '날짜', key: 'rgstDt' },
+];
+
+const SearchBar = ({ searchMap, setSearchMap }) => {
+  const [ csvData, setCsvData ] = useState([]);
+
+  const getDataForCsv = useCallback((searchMap) => {
+    axios.get("/api/get/csv", {params: {moduleIdx: searchMap.moduleIdx,
+                                        beginDate: getYMDAndHourFormatDate(searchMap.beginDate),
+                                        endDate: getYMDAndHourFormatDate(searchMap.endDate)}})
+    .then(response => {
+      setCsvData(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }, [])
+
   const updateSearchMap = useCallback((key, value) => {
     setSearchMap(searchMap => {
+      getDataForCsv({
+        ...searchMap,
+        [key]: value
+      });
+
       return {
         ...searchMap,
         [key]: value
       }
     })
-  }, [setSearchMap])
+  // eslint-disable-next-line no-use-before-define
+  }, [getDataForCsv, setSearchMap])
 
   return (
     <SearchBarConatiner>
@@ -20,9 +55,13 @@ const SearchBar = ({ setSearchMap }) => {
         <SearchDate title='시작날짜' updateSearchMap={updateSearchMap} name='beginDate'/>
         <SearchDate title='종료날짜' updateSearchMap={updateSearchMap} name='endDate'/>
         <SearchModule updateSearchMap={updateSearchMap} />
-        <div className="search-condition col-xl-4 col-sm-6">
-          <button>파일 저장</button>
-        </div>
+        <CSVLink className="search-condition col-xl-4 col-sm-6" 
+                 target="_blank" 
+                 filename={`가스 상세 데이터_${getYMDAndHourFormatDate(searchMap.beginDate)} ~ ${getYMDAndHourFormatDate(searchMap.endDate)}.csv`}
+                 data={csvData}
+                 headers={headers}>
+          파일 저장
+        </CSVLink>
       </div>
     </SearchBarConatiner>
   )
