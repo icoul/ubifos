c1c35536713e9d67d9b9aeb7adae1b3e9a8f4ac9
unknown <icoul35@gmail.com>
2020-12-04 15:31:01 +0900

    메시지 결과 대응

--- src/main/java/com/portable/mornitoring/MornitoringApplication.java
+++ src/main/java/com/portable/mornitoring/MornitoringApplication.java
@@ -1,21 +1,34 @@
 package com.portable.mornitoring;
 
-import java.io.IOException;
-
+import com.portable.mornitoring.controller.SerialController;
 import com.portable.mornitoring.serial.AbstractSpringSerialPortConnector;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MornitoringApplication extends AbstractSpringSerialPortConnector {
+	@Autowired
+	SerialController serialController;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MornitoringApplication.class, args);
 	}
 
+	Runtime rt = Runtime.getRuntime();
+	Process pc = null;
+
 	@Override
 	public void processData(String line) {
 		System.out.println("결과 수신 : " + line);
+
+		if (line.contains("PW+OFF")) {
+			try {
+				pc = rt.exec("sudo shutdown 0");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
