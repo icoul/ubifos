dc736c27ed9dd1518d4a5e9935c0fa70fe05bdda
unknown <icoul35@gmail.com>
2020-12-06 15:01:54 +0900

    어플리케이션 실행 시 커맨드라인 추가

---
+++ src/main/java/com/portable/mornitoring/service/ApplicationService.java
@@ -0,0 +1,21 @@
+package com.portable.mornitoring.service;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationService implements CommandLineRunner {
+  Runtime rt = Runtime.getRuntime();
+  Process pc = null;
+  
+  @Override
+  public void run(String... arg) {
+    System.out.println("application start!");
+
+    try {
+      pc = rt.exec("sudo killall mm");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
