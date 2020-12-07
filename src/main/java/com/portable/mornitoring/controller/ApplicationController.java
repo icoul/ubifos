009033a2deef78e9aff389d6211f39d00236530c
unknown <icoul35@gmail.com>
2020-12-07 13:26:55 +0900

    ip조회함수 수정완료

--- src/main/java/com/portable/mornitoring/controller/ApplicationController.java
+++ src/main/java/com/portable/mornitoring/controller/ApplicationController.java
@@ -1,7 +1,10 @@
 package com.portable.mornitoring.controller;
 
-import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApplicationController {
   @GetMapping(path = "/api/get/ip")
-  public String home() throws UnknownHostException {
-		return Inet4Address.getLocalHost().getHostAddress();
+  public String home() throws UnknownHostException, SocketException {
+    NetworkInterface networkInterface = NetworkInterface.getByName("eth0");
+    Enumeration<InetAddress> inetAddress = networkInterface.getInetAddresses();
+    InetAddress currentAddress;
+    currentAddress = inetAddress.nextElement();
+    return currentAddress.getHostAddress();
 	}
 }
