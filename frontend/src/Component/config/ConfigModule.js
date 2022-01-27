c21fb988d2d22f1f58fa8a56f14328aa19632b33
LGU <gocaps11@gmail.com>
2022-01-27 16:09:04 +0900

    장치관리(등록, 수정, 삭제) 페이지 추가 및 센서명 수정시 공백 입력불가 처리

---
+++ frontend/src/Component/config/ConfigModule.js
@@ -0,0 +1,120 @@
+import Axios from "axios";
+import Modal from "component/config/modal/Modal";
+import moment from "moment";
+import React, { useCallback, useEffect, useState } from "react";
+import { Styles } from "utils/table/Table.css";
+import ModuleRegister from "./ModuleRegister";
+
+const ConfigModule = () => {
+  const [ data, setData ] = useState([]);
+  const [ modalOpen, setModalOpen ] = useState(false);
+  const [ modalHeader, setModalHeader ] = useState();
+  const [ modalData, setModalData ] = useState();
+  const [ modalDataSet, setModalDataSet ] = useState(false);
+  const [ modalType, setModalType ] = useState(true);
+
+  const openModal = (index, type) => {
+    if (type) setModalHeader("장치 정보 수정");
+    else setModalHeader("장치 삭제");
+    setModalOpen(true);
+    setModalData(data[index]);
+    setModalDataSet(true);
+    setModalType(type);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const dataSw = () => {
+    setModalDataSet(!modalDataSet);
+  }
+
+  const getData = useCallback(() => {
+    Axios.get("/api/get/all/modules")
+    .then(response => {
+      setData(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }, [])
+
+  useEffect(() => {
+    getData();
+  }, [getData])
+
+  return (
+    <section>
+      <Modal open={modalOpen} close={closeModal} header={modalHeader} data={modalData} dataSet={dataSw} dataState={modalDataSet} type={modalType}/>
+      <Styles tableType="editable">
+        <ModuleRegister />
+        <div className="table-container">
+          <table role="table">
+            <thead>
+              <tr role="row">
+                <th colSpan="12" role="columnheader">장치정보</th>
+              </tr>
+              <tr role="row">
+                <th role="columnheader"></th>
+                <th role="columnheader">장치번호</th>
+                <th role="columnheader">고유번호</th>
+                <th role="columnheader">시리얼</th>
+                <th role="columnheader">장치명</th>
+                <th role="columnheader">검지유형</th>
+                <th role="columnheader">관리자</th>
+                <th role="columnheader">등록자</th>
+                <th role="columnheader">등록일</th>
+                <th role="columnheader"></th>
+                <th role="columnheader"></th>
+                <th role="columnheader"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((datas, index) => {
+                return (
+                  (datas.delFlag === 0 ?
+                  <tr role="row" key={datas.moduleIdx}>
+                    <td className="cellIndex" role="cell">{index + 1}</td>
+                    <td className="moduleIndex" role="cell">{datas.moduleIdx}</td>
+                    <td className="modelNumber" role="cell">{datas.modelNo}</td>
+                    <td className="modelSerial" role="cell">{datas.modelSerial}</td>
+                    <td className="modelName" role="cell">{datas.modelNm}</td>
+                    <td className="detectKinds" role="cell">{ScnToString(datas.moduleScn)}</td>
+                    <td className="userName" role="cell">{datas.userNm}</td>
+                    <td className="registerId" role="cell">{datas.rgstId}</td>
+                    <td className="registerDate" role="cell">{moment(datas.rgstDt).format("YYYY-MM-DD")}</td>
+                    <td className="btnBox" role="cell"></td>
+                    <td className="btnBox" role="cell">
+                      <div>
+                        <button className="table-search-submit btn btn-info" onClick={() => { openModal(index, true); }}>수정</button>
+                      </div>
+                    </td>
+                    <td className="btnBox" role="cell">
+                      <div>
+                        <button className="table-search-submit btn btn-info" onClick={() => { openModal(index, false); }}>삭제</button>
+                      </div>
+                    </td>
+                  </tr>
+                  : <></>)
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </Styles>
+    </section>
+  )
+}
+
+export default ConfigModule;
+
+const ScnToString = (moduleScn) => {
+  switch (moduleScn) {
+    case 0:
+      return "가스";
+  
+    default:
+      return "미확인";
+  }
+}
---
+++ frontend/src/Component/config/ModuleRegister.js
@@ -0,0 +1,97 @@
+import Axios from "axios";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { BiSave } from "react-icons/bi";
+import { ButtonBarConatiner } from "./ButtonBar.css";
+import { FormContainer } from "./ModuleRegister.css";
+
+const ModuleRegister = () => {
+  const [ formBoxFlag, setFormBoxFlag ] = useState(true);
+  const { register, handleSubmit, setValue } = useForm();
+
+  const handleClick = () => {
+    if (!formBoxFlag) {
+      setValue("modelNo", "");
+      setValue("modelSerial", "");
+      setValue("modelNm", "");
+      setValue("userNm", "");
+      setValue("rgstId", "");
+    }
+    setFormBoxFlag((formBoxFlag) => {
+      return !formBoxFlag;
+    })
+  }
+
+  const onSubmit = (data) => {
+    data.moduleScn = 0;
+    Axios.post( '/api/module/insertData',
+      {
+        moduleIdx : null,
+        ...data,
+        rgstDt : new Date(),
+        delFlag : 0
+      },
+      {
+        headers : {
+          'Content-type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    ).then((response) => {
+      if (response !== null && response.data === "success") {
+        alert('등록 완료');
+        window.location.reload(false);
+      } else if (response.data === "duplicate") {
+        alert('이미 등록된 고유번호 혹은 장치명입니다');
+      } else {
+        alert('등록 실패');
+        console.log(response);
+      }
+    })
+    .catch((response) => {
+      alert('등록 실패');
+      console.log(response);
+    });
+  };
+
+  return (
+    <ButtonBarConatiner>
+      <div className="buttonBar-container">
+        <button className="search-condition csv-btn col-xl-4 col-sm-6" onClick={() => { handleClick(); }}>{(formBoxFlag ? "등록" : "등록취소")}</button>
+      </div>
+      <FormContainer>
+        <div className="form-container" style={{display:(formBoxFlag ? 'none' : 'block')}}>
+          <form className="table-form" onSubmit={handleSubmit(onSubmit)}>
+            <table role="table">
+              <thead>
+                <tr>
+                  <th>고유번호</th>
+                  <th>시리얼</th>
+                  <th>장치명</th>
+                  <th>검지유형</th>
+                  <th>관리자</th>
+                  <th>등록자</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><input type="text" {...register("modelNo")} /></td>
+                  <td><input type="text" {...register("modelSerial")} /></td>
+                  <td><input type="text" {...register("modelNm")} /></td>
+                  <td><input type="text" {...register("moduleScn")} value={"가스"} className="nonEditable" /></td>
+                  <td><input type="text" {...register("userNm")} /></td>
+                  <td><input type="text" {...register("rgstId")} /></td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="uploadBox">
+              <button type="submit"><BiSave /></button>
+            </div>
+          </form>
+        </div>
+      </FormContainer>
+    </ButtonBarConatiner>
+  )
+}
+
+export default ModuleRegister
